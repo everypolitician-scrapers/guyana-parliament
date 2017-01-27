0e42b5ebc3e2917a6d25a9b7b0a36bbbc9bfc580
Tony Bowden <tony@tmtm.com>
2017-01-27 14:07:09 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,7 +12,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -20,7 +21,7 @@ def noko_for(url)
 end
 
 def party_from(text)
-  return ['unkown', 'Unknown'] if text.to_s.empty?
+  return %w(unkown Unknown) if text.to_s.empty?
   return ['APNU-AFC', 'A Party For National Unity + Alliance For Change'] if text.include?('APNU') or text.include?('AFC')
   return ['PPP-Civic', 'People Progressive Party/Civic'] if text.include?('People Progressive Party') or text.include?('Civic')
   warn "Unknown party: #{text}"
@@ -29,17 +30,17 @@ end
 def region_from(text)
   return ['', ''] if text.to_s.empty?
   if matched = text.match(/Region (\d+) - (.*)/)
-    return matched.captures 
+    return matched.captures
   end
   warn "Unknown region: #{text}"
-  return ['', '']
+  ['', '']
 end
 
 class NameParts
   @@prefixes = %w(Assoc Prof Professor Rev Bishop Prince Dr Lt Col Colonel).to_set
   @@prefixes.merge @@male = %w(Mr)
   @@prefixes.merge @@female = %w(Mrs Ms Miss)
-  @@prefixes << "(Ret’)"
+  @@prefixes << '(Ret’)'
 
   @@gender_map = Hash[@@female.map { |e| [e, 'female'] }].merge(Hash[@@male.map { |e| [e, 'male'] }])
 
@@ -53,7 +54,7 @@ class NameParts
 
   def name
     # .sub called for specific case where Dr. is not separated from name by space
-    partitioned.last.sub('Dr.Bharrat','Bharrat')
+    partitioned.last.sub('Dr.Bharrat', 'Bharrat')
   end
 
   def gender
@@ -61,6 +62,7 @@ class NameParts
   end
 
   private
+
   def words
     @_words ||= @orig.split(/\s/)
   end
@@ -89,7 +91,7 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   noko.css('.who-parliament .member-image .swap-title a/@href').map(&:text).each do |link|
-    scrape_mp(URI.join url, link)
+    scrape_mp(URI.join(url, link))
   end
 end
 
@@ -97,24 +99,24 @@ def scrape_mp(url)
   noko = noko_for(url)
   party_id, party   = party_from noko.xpath('.//span[@class="meta-title" and contains(.,"Party")]/following-sibling::text()').text.tidy
   region_id, region = region_from noko.xpath('.//span[@class="meta-title" and contains(.,"Region")]/following-sibling::a').text.tidy
-  nameparts = NameParts.new( noko.css('div.bread-crumb li.last').text.tidy.sub(/^Hon.? /,'') )
-  data = { 
-    id: url.to_s.split("/").last,
+  nameparts = NameParts.new(noko.css('div.bread-crumb li.last').text.tidy.sub(/^Hon.? /, ''))
+  data = {
+    id:               url.to_s.split('/').last,
     honorific_prefix: nameparts.prefix,
-    name: nameparts.name,
-    gender: nameparts.gender,
-    role: noko.xpath('.//span[@class="meta-title" and contains(.,"Designation")]/following-sibling::text()').text.tidy,
-    party_id: party_id,
-    party: party,
-    area_id: region_id,
-    area: region,
-    term: 11,
-    image: noko.css('.dep-head .member-image img.border/@src').text.sub('__small',''),
-    source: url.to_s,
+    name:             nameparts.name,
+    gender:           nameparts.gender,
+    role:             noko.xpath('.//span[@class="meta-title" and contains(.,"Designation")]/following-sibling::text()').text.tidy,
+    party_id:         party_id,
+    party:            party,
+    area_id:          region_id,
+    area:             region,
+    term:             11,
+    image:            noko.css('.dep-head .member-image img.border/@src').text.sub('__small', ''),
+    source:           url.to_s,
   }
-  # data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
-  warn [data[:honorific_prefix], data[:name], data[:gender]].join(" ----- ") 
-  ScraperWiki.save_sqlite([:id, :term], data)
+  #  data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
+  warn [data[:honorific_prefix], data[:name], data[:gender]].join(' ----- ')
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('http://parliament.gov.gy/about-parliament/parliamentarian')
